15476ef2927d97c4cddb29cad4e7d452b823d95b
QuixoteHY <1290482442@qq.com>
2019-07-24 09:21:03 +0800

    tree shaking

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,24 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+//import _ from 'lodash';
+//import printMe from './print.js';
+//import './styles.css';
+import { cube } from './math.js';
 
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+//  var element = document.createElement('div');
+//  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+//  btn.innerHTML = 'Click me and check the console!';
+//  btn.onclick = printMe;
+//  element.appendChild(btn);
 
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
